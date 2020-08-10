db5dbf70de4f58c8d76f3d9b29c956c2d342d166
xiote <xiote@nate.com>
2020-08-11 04:21:15 +0900

    update

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,18 +2,17 @@ $(document).ready(function() {
     $('#tagname').focus();
     $('.message').hide();
 
-    $('#search').submit(function (event) {
-      event.preventDefault();
-      var tagname = $('#tagname').val().toLowerCase();
-      $('#tagname').val("");
-      $('.posts, .message').hide();
-      if (tagname=="") {
-        $('.posts').show();
-              } else {
-      $('.posts[data-name*="'+tagname+'"]').show();
-                      }
-      if ($('.posts:visible').length < 1) {
-        $('.message').show();
-      }
-    });
+});
+
+$('#tagname').live('keyup', function() {
+  var tagname = $('#tagname').val().toLowerCase();
+  $('.posts, .message').hide();
+  if (tagname=="") {
+    $('.posts').show();
+  } else {
+    $('.posts[data-name*="'+tagname+'"]').show();
+  }
+  if ($('.posts:visible').length < 1) {
+    $('.message').show();
+  }
 });
